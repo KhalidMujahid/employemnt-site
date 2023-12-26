424ca1a397e26b08e28de3f055bf772aae7c32e0
KhalidMujahid <binkhalid267@gmail.com>
2023-12-26 21:48:35 +0100

    new

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const path = require("path");
 const helmet = require("helmet");
+const cors = require("cors");
 const router = require("./routes/router");
 
 const app = express();
 
-app.set("view engine","ejs");
+//app.set("view engine","ejs");
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,"../","public")));
+//app.use(express.static(path.join(__dirname,"../","public")));
 app.use(helmet());
+app.use(cors());
 
 // routes
 app.use("/",router);
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,49 +2,13 @@ const router = require("express").Router();
 const Contact = require("../models/Contact");
 const Job = require("../models/Job");
 
-// render index page
-router.get("/", async (req,res,next) => {
-	try {
-		res.status(200).render("index");
-	} catch(e) {
-		next(e);
-	}
-});
-
-// render jobs page
-router.get("/jobs", async (req,res,next) => {
-	try {
-		res.status(200).render("jobs",{
-			show: false,
-			message:""
-		});
-	} catch(e) {
-		next(e);
-	}
-});
-
-// render contact page
-router.get("/contact", async (req,res,next) => {
-	try {
-		res.status(200).render("contact",{
-			show: false,
-			message: ""
-		});
-	} catch(e) {
-		next(e);
-	}
-});
-
 // apply job
 router.post("/apply", async (req,res,next) => {
 	try {
 		const { fullname,email,address,state,city,zipcode,cell,dbo,occupation } = req.body;
 
 		if(!fullname || !email || !address || !city || !zipcode || !cell || !dbo || !occupation){
-			return res.status(400).render("jobs",{
-			   show: true,
-			   message:"All inputs are required!"
-		    });
+			return res.status(400).send({ message:"All inputs are required!" });
 		}
 
        const response = await Job.create({
@@ -60,15 +24,9 @@ router.post("/apply", async (req,res,next) => {
        });
 
        if(response){
-       	return res.status(201).render("jobs",{
-			show: true,
-			message: "Application submitted successfully!"
-		});
+       	return res.status(201).send({ message: "Application submitted successfully!" });
        } else {
-       	return res.status(400).render("jobs",{
-			show: true,
-			message: "An error occurred!"
-		});
+       	return res.status(400).send({ message: "An error occurred!" });
        }
 
 	} catch(e) {
@@ -84,24 +42,15 @@ router.post("/contact", async (req,res,next) => {
 		const { name,email, subject, message } = req.body;
 
 		if(!name || !email || !subject || !message) {
-			return res.status(400).render("contact",{
-				show: true,
-				message: "All inputs are required!"
-			});
+			return res.status(400).send({ message: "All inputs are required!" });
 		}
 
 		const response = await Contact.create({ name, email, subject, message});
 
 		if(response){
-			return res.status(201).render("contact",{
-				show: true,
-				message: "Message sent!"
-			});
+			return res.status(201).send({ message: "Message sent!" });
 		} else {
-			return res.status(400).render("contact",{
-				show: true,
-				message: "An error occurred!"
-			});
+			return res.status(400).send({ message: "An error occurred!" });
 		}
 
 	} catch(e) {
